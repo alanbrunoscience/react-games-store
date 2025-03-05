b6d036faca0dac5b41934198fc59a17ed31675ba
alanbrunoscience <alanbrunomath@gmail.com>
2025-03-04 22:29:51 -0300

    :sparkles: Finish CardProdutos component

--- src/components/produtos/cardprodutos/CardProdutos.tsx
+++ src/components/produtos/cardprodutos/CardProdutos.tsx
@@ -8,27 +8,34 @@ interface CardProdutosProps {
 
 function CardProdutos({ produto }: CardProdutosProps) {
   return (
-    <div className="flex flex-col justify-between overflow-hidden border rounded-2xl bg-white">
+    <div className="flex flex-col justify-between overflow-hidden drop-shadow-xl rounded-xl bg-white h-full transition-transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg">
 
-      <div className="flex justify-end items-center w-full py-2 gap-2" >
+      <div className="flex justify-end items-center w-full py-2 px-2 gap-2" >
         <Link to={`/editarcategoria/${produto.id}`}>
-          <button><Pencil size={24} /></button>
+          <button><Pencil size={24} className="mr-1 hover:fill-teal-800" /></button>
         </Link>
 
         <Link to={`/deletarcategoria/${produto.id}`}>
-          <button><Trash size={24} /></button>
+          <button><Trash size={24} className="mr-1 hover:fill-red-700" /></button>
         </Link>
       </div>
 
-      <div className="gap-1">
-        <img src={`${produto.foto}`} alt="Game cover" />
-        <p>{produto.nome.toUpperCase()}</p>
-        <p className="font-bold">{`R$ ${produto.preco}`}</p>
-        <p className="italic">{`Categoria: ${produto.categoria}`}</p>
+      <div className="flex flex-col gap-1 px-2 py-4 justify-center items-center">
+        <img src={`${produto.foto}`} alt="Game cover" className="mx-auto my-1 h-64 max-w-75" />
+        <p className="text-base lg:text-lg uppercase">{produto.nome}</p>
+        <h3 className="text-lg lg:text-xl font-bold uppercase">
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+          }).format(produto.preco)}
+        </h3>
+        <p className="text-base lg:text-lg italic">{`Categoria: ${produto.categoria?.tipo}`}</p>
       </div>
 
-      <div className="bg-teal-600">
-        <p className="px-6 py-2 text-2xl font-bold text-white">Categoria</p>
+      <div className="flex flex-wrap">
+        <button className="flex items-center justify-center w-full py-2 text-white bg-teal-600 hover:bg-teal-900 text-base">
+          Comprar
+        </button>
       </div>
 
     </div>
